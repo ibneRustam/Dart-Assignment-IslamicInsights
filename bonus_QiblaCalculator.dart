3c0aefd2bbdb5a46b1ee5898af8b760356e46fbf
ibneRustam <abdulqadeerrustam786@gmail.com>
2025-04-07 20:23:19 +0500

    Qibla Calculator done

--- bonus_QiblaCalculator.dart
+++ bonus_QiblaCalculator.dart
@@ -1,23 +1,45 @@
 import 'dart:io';
+import 'dart:math';
 
 void main() {
   // Makkah location
-  const double makkahLat = 21.4225;
-  const double makkahLong = 39.8262;
+  double makkahLat = 21.4225;
+  double makkahLong = 39.8262;
 
-  print("Enter Your Latitude here :");
+  print("Enter Your City Latitude here :");
 
-
-  // user latitude 
-  String? latInput = stdin.readLineSync(); 
+  // user latitude
+  String? latInput = stdin.readLineSync();
   double userLat = double.parse(latInput!);
 
-
-
-
-  // user longitude 
-  print("Enter Your Longitude here :"); 
+  // user longitude
+  print("Enter Your City Longitude here :");
   String? longInput = stdin.readLineSync();
-
   double userLong = double.parse(longInput!);
+
+  // dono values ko radinas se badalny k lye
+  double latMakkahRad = makkahLat * pi / 180;
+  double longMakkahRad = makkahLong * pi / 180;
+
+  double latUserlat = userLat * pi / 180;
+  double longUserLong = userLong * pi / 180;
+
+  // direction malom krny k lye
+
+  double directionLat = latMakkahRad - latUserlat;
+  double directionLong = longMakkahRad - longUserLong;
+
+  double a = sin(directionLong) * cos(latMakkahRad);
+  double b =
+      cos(latUserlat) * sin(latMakkahRad) -
+      sin(latUserlat) * cos(latMakkahRad) * cos(directionLong);
+
+  double qiblaAngle = atan2(a, b);
+
+  double qibleAngelDegree = qiblaAngle * 180 / pi;
+
+  if (qibleAngelDegree < 0) {
+    qibleAngelDegree += 360;
+  }
+  print("Qibla Direction : $qibleAngelDegree");
 }
